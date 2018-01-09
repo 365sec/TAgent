750e21292ab15fdf838ab8d6648bd50a1375e2fa
chushuai <1040641785@qq.com>
2018-01-09 13:56:46 +0800

    处理exc_cmd指令

--- worker.py
+++ worker.py
@@ -5,6 +5,7 @@ from bson.binary import STANDARD
 from bson.codec_options import CodecOptions
 from command import *
 from syslog_log import *
+import os
 
 class Worker(threading.Thread):
     def __init__(self,recv_queue,send_queue):
@@ -14,21 +15,33 @@ class Worker(threading.Thread):
         self.send_queue = send_queue
         
     
+    def __process_exc_cmd(self,data_json):
+        try:
+            cmdline = data_json.get('exc_cmd').get('cmdline')
+            output = os.popen(cmdline)
+            self.send_queue.put(AgentServerCommandExc("".join(output.readlines())).to_bson())
+        except Exception as err:
+            print err
+            
     def __process_bson_control_message(self, data):
         data_json = BSON.decode(BSON(data), codec_options=CodecOptions(uuid_representation=STANDARD))
         print data_json
         if data_json.get('exc_cmd', None):
-            t = data_json.get('exc_cmd').get('cmdline')
-            info('exc_cmd request with timestamp %s' % t)
-            self.send_queue.put(AgentServerCommandExc().to_bson())
-
+            self.__process_exc_cmd(data_json)
         else:
             warning("Unknown BSON command: '%s'" % str(data_json))
     
     
     def run(self):
-        print "Workering ..."
+        print "Worker"
         while True:
            packet = self.recv_queue.get()
            self.__process_bson_control_message(packet)
         
+
+
+if __name__ == "__main__":
+    output = os.popen('ifconfig')
+    
+   
+    print "".join(output.readlines())
