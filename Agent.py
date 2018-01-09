abaa3a8225a2a6e803ec75c75e79368a9a522148
chushuai <1040641785@qq.com>
2018-01-09 13:55:35 +0800

    通过配置文件加载配置信息

--- Agent.py
+++ Agent.py
@@ -7,6 +7,9 @@ from jinja2.ext import do
 import Queue
 from worker import Worker
 from sender import Sender
+from heartbeat import Heartbeat
+from Config import  Conf
+import os
 from __builtin__ import True
 
 class Agent():
@@ -14,10 +17,10 @@ class Agent():
     def __init__(self):
         self.options = CommandLineOptions().get_options()
         self.server_connection  = None
-        
         self.send_packet_queue = Queue.Queue(maxsize=0)
         self.recv_packet_queue = Queue.Queue(maxsize=0)
-
+        self.config = Conf()
+        self.config.read([os.path.join(os.path.dirname(os.path.realpath(__file__)),"config.cfg")])
 
     
     def working(self):
@@ -33,39 +36,44 @@ class Agent():
         return buffer_data[0:message_length],buffer_data[message_length:len]
     
     def __start_agent(self):
-       server_ip = "172.16.39.185"
-       #server_ip = "127.0.0.1"
-       server_port = 5678 
-       priority = 1
-       sensor_id= "f12211e7-9ea8-000c-29f6-3d1ce094b5dc"
-       system_id_file = ""   
+       server_ip = self.config.get("server","ip")
+       server_port = int(self.config.get("server","port"))
+       sensor_id= self.config.get("agent","id")
        
+       priority = 1
+       system_id_file = ""   
+ 
        Worker(self.recv_packet_queue,self.send_packet_queue).start()
       
        self.server_connection = ServerConn( server_ip, server_port, priority, sensor_id,system_id_file)
-       self.server_socket = self.server_connection.connect()
-       if self.server_socket == None :
+       server_socket = self.server_connection.connect()
+       if server_socket == None :
            print "connect failed !"
            exit(0);
            
-       Sender(self.send_packet_queue,self.server_socket).start()
-       self.server_socket.setblocking(False)
+       Sender(self.send_packet_queue,self.server_connection).start()
+       Heartbeat(self.send_packet_queue).start()
+       
+       server_socket.setblocking(False)
        read_buff = ""
        print "===="
        while True:
-          readable, writable, exceptional = select.select([self.server_socket], [], [self.server_socket])
+          readable, writable, exceptional = select.select([self.server_connection.get_connectsocket()], [], [self.server_connection.get_connectsocket()])
           #handle read event
-          for s in readable :
-              #construct packet
-              chunk= s.recv(1024*1024)
-              read_buff += chunk
-              read_buff_len = len(read_buff)
-              while( read_buff_len > 4) :
-                 packet, read_buff = self.get_bson_packet(read_buff,read_buff_len)
-                 if packet:
-                   print "push a packet"
-                   self.recv_packet_queue.put(packet)
-                 read_buff_len = len(read_buff)
+          try:
+              if server_socket in readable :
+                  #construct packet
+                  chunk= server_socket.recv(1024*1024)
+                  read_buff += chunk
+                  read_buff_len = len(read_buff)
+                  while( read_buff_len > 4) :
+                     packet, read_buff = self.get_bson_packet(read_buff,read_buff_len)
+                     if packet:
+                       print "push a packet"
+                       self.recv_packet_queue.put(packet)
+                     read_buff_len = len(read_buff)
+          except  Exception as e:
+               print e
                  
                  
           for s in exceptional:
